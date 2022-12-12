8c0ef5711471cf95a85d21f9de389cc5f2236b51
nixissmtp <gleb@simple2b.(none)>
2022-12-13 00:02:05 +0200

    first commit

---
+++ s2b_task/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa F401
+from .task_executor import task, execute_task
+from .exc import TaskAlreadyExists
---
+++ tests/test_tasks.py
@@ -0,0 +1,28 @@
+from s2b_task import task, execute_task
+from s2b_task.task_executor import TaskNotExists
+from s2b_task.storage import TASK_MAP
+
+
+def test_task_registration():
+    assert not TASK_MAP
+
+    @task
+    def foo():
+        """I'm foo"""
+        print("foo")
+
+    @task
+    def bar():
+        """I'm bar"""
+        print("bar")
+
+    assert TASK_MAP
+
+    for task_name in ("foo", "bar"):
+        execute_task(task_name)
+
+    try:
+        execute_task(("_", "invalid"))
+        assert False, "Non existing task found"
+    except TaskNotExists:
+        ...
